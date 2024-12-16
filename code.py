d90d9f7656cefe72b931c004f6e190686ab27801
olbapeurt <166437619+olbapeurt@users.noreply.github.com>
2024-12-16 22:41:57 +0200

    Add files via upload

---
+++ code.py
@@ -0,0 +1,26 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2758
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 <?xml version="1.0" encoding="UTF-8"?>\
+<module type="PYTHON_MODULE" version="4">\
+  <component name="NewModuleRootManager">\
+    <content url="file://$MODULE_DIR$">\
+      <excludeFolder url="file://$MODULE_DIR$/venv" />\
+    </content>\
+    <orderEntry type="jdk" jdkName="Python 3.7" jdkType="Python SDK" />\
+    <orderEntry type="sourceFolder" forTests="false" />\
+    <orderEntry type="module" module-name="SquareRootMethod" />\
+    <orderEntry type="module" module-name="Test" />\
+    <orderEntry type="module" module-name="Bank" />\
+  </component>\
+  <component name="TestRunnerService">\
+    <option name="PROJECT_TEST_RUNNER" value="Unittests" />\
+  </component>\
+</module>\
+}
